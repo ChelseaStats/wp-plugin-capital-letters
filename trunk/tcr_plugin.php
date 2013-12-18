72589d66785610b513fec3bef45087682200ae2f
ChelseaStats <ChelseaStats@users.noreply.github.com>
2013-12-18 08:32:45 +0000

    Update tcr_plugin.php

--- trunk/tcr_plugin.php
+++ trunk/tcr_plugin.php
@@ -8,8 +8,8 @@ Author: The Cellar Room Limited
 Author URI: http://www.thecellarroom.net
 Donation URI: http://thecellarroom.net/plugins
 Copyright (c) 2013 The Cellar Room Limited
-Description: Simply auto uppercases the first letter of the first word after finding a full stop in your post content. 
-             No options or settings, works out of the box, simply just activate.
+Description: Simply auto uppercases the first letter of the first word after finding a full stop in your post content.
+auto fixes post titles too. No options or settings, works out of the box, simply just activate.
 */
 ###################################################################################
 defined( 'ABSPATH' ) or die();
@@ -20,6 +20,27 @@ $content=preg_replace_callback( '|(?:\.)(?:\s*)(\w{1})|Ui',create_function('$mat
 return $content;
 }
 add_filter('the_content','TCR_CL_replace_content');
+
+
+function lowertitle($title)  {
+$title = strtolower($title);
+return $title;
+}
+
+function fixtitle($title) {
+$smallwordsarray = array( 'of','a','the','and','an','or','nor','but','is','if','then','else','when', 'at','from','by','on','off','for','in','out','over','to','into','with' ); 
+$words = explode(' ', $title); 
+foreach ($words as $key => $word) {
+if ($key == 0 or !in_array($word, $smallwordsarray)) $words[$key] = ucwords($word); 
+} 
+$newtitle = implode(' ', $words); return $newtitle; }
+
+add_filter('title_save_pre','lowertitle');
+add_filter('the_title','lowertitle');
+add_filter('title_save_pre','fixtitle');
+add_filter('the_title','fixtitle');
+
+}
 
 ###################################################################################
 
